c52c707004257f8e042c308f934c81da6fc5bff0
asingh16513 <asingh>
2020-09-15 22:08:59 +0530

    added unit test

--- Domain/Models/UserAuthResult.cs
+++ Domain/Models/UserAuthResult.cs
@@ -5,7 +5,6 @@ namespace Domain.Models
     public class UserAuthResult
     {
         public string AuthToken { get; set; }
-        public Guid RequestId { get; set; }
         public int UserId { get; set; }
     }
 }
